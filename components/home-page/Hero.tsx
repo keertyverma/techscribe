29dc8053cce47a98ef4f5279a67cd1b7f3491248
Keerty Verma <keertyverma1303@gmail.com>
2023-06-19 14:31:10 +0530

    add hero component

---
+++ components/home-page/Hero.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./Hero.module.css";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/profile-pic.jpeg"
+          alt="profile-pic"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hey, I'm Keerty</h1>
+      <p>
+        Discover the world of web development with our tech blog. Stay updated
+        on industry trends and unleash your potential in creating exceptional
+        digital experiences.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import Hero from "@/components/home-page/Hero";
+import React, { Fragment } from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Fragment>
+      <Hero />
+    </Fragment>
+  );
 };
 
 export default HomePage;

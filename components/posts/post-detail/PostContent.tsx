247cfb6877ef3cc908c018523c6ecfd3299e71b6
Keerty Verma <keertyverma1303@gmail.com>
2023-06-20 17:17:20 +0530

    render code snippet from markdown

--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-import PostHeader from "./PostHeader";
-import classes from "./PostContent.module.css";
 import { PostType } from "@/types";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import classes from "./PostContent.module.css";
+import PostHeader from "./PostHeader";
 
 interface Props {
   post: PostType;
+}
+
+interface CodeProps {
+  inline?: boolean;
+  className?: string;
+  children?: React.ReactNode;
 }
 
 const PostContent = ({ post }: Props) => {
@@ -33,6 +41,24 @@ const PostContent = ({ post }: Props) => {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code: CodeProps) {
+      const { inline, className, children, ...props } = code;
+      const match = /language-(\w+)/.exec(className || "");
+
+      return !inline && match ? (
+        <SyntaxHighlighter
+          {...props}
+          children={String(children).replace(/\n$/, "")}
+          style={atomDark}
+          language={match[1]}
+        />
+      ) : (
+        <code {...props} className={className}>
+          {children}
+        </code>
+      );
     },
   };
   return (

82266869ea6d46e3102adcf59b860daba05f4305
Keerty Verma <keertyverma1303@gmail.com>
2023-06-20 17:28:52 +0530

    add contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,8 @@
+import ContactForm from "@/components/contact/ContactForm";
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return <ContactForm />;
 };
 
 export default Contact;

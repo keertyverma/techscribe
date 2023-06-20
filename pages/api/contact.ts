4d2b401e418d61e5bf09af2e9627371444d320ce
Keerty Verma <keertyverma1303@gmail.com>
2023-06-20 18:18:03 +0530

    store feedback in mongodb

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -18,10 +19,30 @@ function handler(req: NextApiRequest, res: NextApiResponse) {
     }
 
     // store it in db
-    const newMessage = { email, name, message };
+    const newMessage = { email, name, message, id: "" };
     console.log("newMessage = ", newMessage);
 
-    res.status(201).json({ message: "Success", data: newMessage });
+    let client;
+    try {
+      client = await MongoClient.connect(
+        process.env.NEXT_MONGODB_URI as string
+      );
+    } catch (err) {
+      return res.status(500).json({ message: "Couldn't connect to db" });
+    }
+
+    const db = client?.db();
+
+    try {
+      const result = await db?.collection("messages").insertOne(newMessage);
+      newMessage.id = result?.insertedId.toString() as string;
+    } catch (err) {
+      return res.status(500).json({ message: "Storing mnessage failed" });
+    } finally {
+      client?.close();
+    }
+
+    return res.status(201).json({ message: "Success", data: newMessage });
   }
 }
 

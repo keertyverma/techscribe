5c34a43eb7fd9e74aec12c9f9b05399c6c2f9e23
Keerty Verma <keertyverma1303@gmail.com>
2023-06-20 16:45:58 +0530

    rendering next image component from markdown

--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -4,6 +4,7 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./PostHeader";
 import classes from "./PostContent.module.css";
 import { PostType } from "@/types";
+import Image from "next/image";
 
 interface Props {
   post: PostType;
@@ -12,10 +13,32 @@ interface Props {
 const PostContent = ({ post }: Props) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers: object = {
+    p(paragraph: { children?: boolean; node?: any }) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
